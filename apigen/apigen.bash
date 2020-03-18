f7f40d5e6537f335cd2201f365585aed4a0abad4
Kiyoshi '13k' Murata <kbmurata@gmail.com>
2020-03-18 17:32:03 -0300

    apigen: remove build script

--- apigen/apigen.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -eo pipefail
-
-echo "This script should be run in GOPATH due to go-mod compatibility issues."
-GO111MODULE=on go build -o apigen ./
-GO111MODULE=on go mod vendor
-GO111MODULE=off ./apigen generate-api
