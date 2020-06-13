75b59d6951ad08e286359d33e29f93c164bf9aee
Alireza Josheghani <josheghani.dev@gmail.com>
2020-06-13 11:56:52 +0430

    Adding RespondPartyInvite method to respond to a party invite

--- client_party.go
+++ client_party.go
@@ -8,3 +8,11 @@ import (
 func (d *Dota2) LeaveParty() {
 	d.write(uint32(bgcm.EGCBaseMsg_k_EMsgGCLeaveParty), &bgcm.CMsgLeaveParty{})
 }
+
+// RespondPartyInvite attempts to respond to a party invite.
+func (d *Dota2) RespondPartyInvite(partyId uint64, accept bool) {
+	d.write(uint32(bgcm.EGCBaseMsg_k_EMsgGCPartyInviteResponse), &bgcm.CMsgPartyInviteResponse{
+		PartyId: &partyId,
+		Accept: &accept,
+	})
+}
